849bb3b700bd20e1afe026d115a591b0f3db9f18
TNTsky <92364781+TNTsky@users.noreply.github.com>
2026-03-02 02:33:35 +0800

    Clean up comments in resource_path function
    Remove unnecessary comment about getting absolute path.

--- main.py
+++ main.py
@@ -45,9 +45,7 @@ set_less_life = 100
 
 # 為了打包成exe
 def resource_path(relative_path):
-    """获取程序中所需文件资源的绝对路径"""
     try:
-        # PyInstaller创建临时文件夹,将路径存储于_MEIPASS
         base_path = sys._MEIPASS
     except Exception:
         base_path = os.path.abspath(".")
